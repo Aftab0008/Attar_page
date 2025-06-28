9c770005c7ea5dba914455b30e485a9592f1309a
Muhammad Aftab Alam <143538026+Aftab0008@users.noreply.github.com>
2025-06-28 10:31:15 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js or vite.config.mjs
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss' // ✅ correct import (not '@tailwindcss/vite')
-import autoprefixer from 'autoprefixer' // optional, for CSS compatibility
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
